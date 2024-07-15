126df58e592efd637ec73f9853d028c277edbc57
Env-byte <46604415+Env-byte@users.noreply.github.com>
2024-07-15 22:07:41 +0100

    Update unit tests to use contexts

--- src/main.test.tsx
+++ src/main.test.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { useStateStepper } from './main';
 import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { beforeEach, describe, expect } from 'vitest';
+import { beforeEach, describe, expect, TestAPI } from 'vitest';
 import { Step } from './step';
 
 const consoleLogSpy = vi.spyOn(console, 'log');
@@ -53,87 +53,111 @@ const DummyComponent = ({ steps }: { steps: Step<string>[] }) => {
     );
 };
 
-describe('Main', () => {
-    let prevButton: HTMLElement;
-    let nextButton: HTMLElement;
-    let renderCount: HTMLElement;
-    let currentStep: HTMLElement;
-    let counterButton: HTMLElement;
+interface DummyComponent {
+    component: (steps: Step<string>[]) => {
+        prevButton: HTMLElement;
+        nextButton: HTMLElement;
+        renderCount: HTMLElement;
+        currentStep: HTMLElement;
+        counterButton: HTMLElement;
+    };
+}
 
+const dummyComponentTest = test.extend<DummyComponent>({
+    component: async ({}, use) => {
+        await use((steps) => {
+            render(<DummyComponent steps={steps} />);
+            return {
+                prevButton: screen.getByTestId('prev-button'),
+                nextButton: screen.getByTestId('next-button'),
+                counterButton: screen.getByTestId('counter-button'),
+                renderCount: screen.getByTestId('render'),
+                currentStep: screen.getByTestId('current'),
+            };
+        });
+    },
+});
+
+describe('Main', () => {
     beforeEach(() => {
         vi.clearAllMocks();
     });
 
-    const renderComponent = (steps: Step<string>[]) => {
-        render(<DummyComponent steps={steps} />);
-        prevButton = screen.getByTestId('prev-button');
-        nextButton = screen.getByTestId('next-button');
-        counterButton = screen.getByTestId('counter-button');
-        renderCount = screen.getByTestId('render');
-        currentStep = screen.getByTestId('current');
-    };
-
     describe('manual steps', () => {
-        it('should progress via buttons, ensuring hook return is stable', async () => {
-            renderComponent(['a', 'b', 'c']);
-            expect(renderCount.textContent).toBe('1');
-            expect(currentStep.textContent).toBe('a');
-            await userEvent.click(nextButton);
-            expect(renderCount.textContent).toBe('2');
-            expect(currentStep.textContent).toBe('b');
-            await userEvent.click(nextButton);
-            expect(renderCount.textContent).toBe('3');
-            expect(currentStep.textContent).toBe('c');
-            await userEvent.click(prevButton);
-            expect(renderCount.textContent).toBe('4');
-            expect(currentStep.textContent).toBe('b');
-            expect(consoleLogSpy).toHaveBeenCalledTimes(4);
-            await userEvent.click(counterButton);
-            expect(consoleLogSpy).toHaveBeenCalledTimes(4);
-        });
+        dummyComponentTest(
+            'should progress via buttons, ensuring hook return is stable',
+            async ({ component }) => {
+                const {
+                    renderCount,
+                    prevButton,
+                    currentStep,
+                    nextButton,
+                    counterButton,
+                } = component(['a', 'b', 'c']);
+                expect(renderCount.textContent).toBe('1');
+                expect(currentStep.textContent).toBe('a');
+                await userEvent.click(nextButton);
+                expect(renderCount.textContent).toBe('2');
+                expect(currentStep.textContent).toBe('b');
+                await userEvent.click(nextButton);
+                expect(renderCount.textContent).toBe('3');
+                expect(currentStep.textContent).toBe('c');
+                await userEvent.click(prevButton);
+                expect(renderCount.textContent).toBe('4');
+                expect(currentStep.textContent).toBe('b');
+                expect(consoleLogSpy).toHaveBeenCalledTimes(4);
+                await userEvent.click(counterButton);
+                expect(consoleLogSpy).toHaveBeenCalledTimes(4);
+            }
+        );
     });
 
-    const testCases: { name: string; steps: Step<string>[] }[] = [
-        {
-            name: 'same timers',
-            steps: [
-                { name: 'a', timer: 200 },
-                { name: 'b', timer: 200 },
-                { name: 'c', timer: 200 },
-            ],
-        },
-        {
-            name: 'different timers',
-            steps: [
-                { name: 'a', timer: 150 },
-                { name: 'b', timer: 200 },
-                { name: 'c', timer: 250 },
-            ],
-        },
-    ];
-
-    test.each(testCases)('with timer - $name', async ({ steps }) => {
-        renderComponent(steps);
-        expect(renderCount.textContent).toBe('1');
-        expect(currentStep.textContent).toBe('a');
-        await waitFor(
-            () => {
-                expect(currentStep.textContent).toBe('b');
+    describe('timer steps', () => {
+        const testCases: { name: string; steps: Step<string>[] }[] = [
+            {
+                name: 'same timers',
+                steps: [
+                    { name: 'a', timer: 200 },
+                    { name: 'b', timer: 200 },
+                    { name: 'c', timer: 200 },
+                ],
             },
             {
-                timeout: 300,
-            }
-        );
-
-        await waitFor(
-            () => {
-                expect(currentStep.textContent).toBe('c');
+                name: 'different timers',
+                steps: [
+                    { name: 'a', timer: 150 },
+                    { name: 'b', timer: 200 },
+                    { name: 'c', timer: 250 },
+                ],
             },
-            {
-                timeout: 300,
+        ];
+
+        dummyComponentTest.for(testCases)(
+            'with timers - $name',
+            async ({ steps }, { component }) => {
+                const { renderCount, currentStep } = component(steps);
+                expect(renderCount.textContent).toBe('1');
+                expect(currentStep.textContent).toBe('a');
+                await waitFor(
+                    () => {
+                        expect(currentStep.textContent).toBe('b');
+                    },
+                    {
+                        timeout: 300,
+                    }
+                );
+
+                await waitFor(
+                    () => {
+                        expect(currentStep.textContent).toBe('c');
+                    },
+                    {
+                        timeout: 300,
+                    }
+                );
+                expect(renderCount.textContent).toBe('3');
+                expect(consoleLogSpy).toHaveBeenCalledTimes(3);
             }
         );
-        expect(renderCount.textContent).toBe('3');
-        expect(consoleLogSpy).toHaveBeenCalledTimes(3);
     });
 });
