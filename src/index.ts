c5b0478b504b864ac28b87faf63755067d4a6070
Env-byte <46604415+Env-byte@users.noreply.github.com>
2024-07-19 20:36:32 +0100

    fix: forgot to export types (#21)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { useStateStepper } from './main';
 
 export { StepConfig, Step, isStepObject, getStepName } from './step';
+export { utils, GetName } from './utils';
 
 export default useStateStepper;
