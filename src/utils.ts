b7588552b83d18b661ccca313b1141599bfbae2b
Env-byte <46604415+Env-byte@users.noreply.github.com>
2024-07-19 20:13:19 +0100

    Added utilities (#14)
    * Unit Tests
    
    * Update unit tests to use contexts
    
    * feat: added utilities

---
+++ src/utils.ts
@@ -0,0 +1,19 @@
+import { Step } from './step';
+
+export type GetName<T extends Step<string>[]> = T extends Step<infer U>[]
+    ? U
+    : never;
+
+export namespace utils {
+    /**
+     * Used to create a list of steps with correct typings returned.
+     * Main usage is to be able to grab the name of the step from the list.
+     * Example:
+     * const steps = utils.create(['a', 'b', 'c']);
+     * type Steps = GetName<typeof steps>; // a | b | c
+     * @param steps
+     */
+    export const create = <Name extends string>(steps: Step<Name>[]) => {
+        return steps;
+    };
+}
